850c721c562d6f701d71ff70dcd8d6c452a100d8
Arnold Taremwa <taremwaarnold@gmail.com>
2017-11-15 10:24:44 +0300

    [Feature #152837704] Add login and register components

---
+++ src/components/__tests__/ShowRegister.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Enzyme, {mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import * as sinon from "sinon";
+import ShowRegister from "../ui/ShowRegister";
+
+Enzyme.configure({adapter: new Adapter()});
+
+
+function setup() {
+    const props = {
+        onRegister: sinon.spy(),
+        user: {},
+        history: {push: sinon.spy()}
+    };
+
+    const enzymeWrapper = mount(<ShowRegister {...props} />);
+
+    return {
+        props,
+        enzymeWrapper
+    }
+}
+
+
+describe("<ShowRegister /> Component", () => {
+
+    it("should render self and sub-components", () => {
+        const {enzymeWrapper} = setup();
+
+        expect(enzymeWrapper.find(".page-header").length).toEqual(1);
+        expect(enzymeWrapper.find("form").length).toEqual(1);
+        expect(enzymeWrapper.find("button").text()).toBe("REGISTER");
+
+
+    });
+
+    it("should make register attemp on form submit", () => {
+        const {enzymeWrapper, props} = setup();
+
+        enzymeWrapper.find("form").simulate("submit");
+        expect(props.onRegister.calledOnce).toEqual(true);
+    });
+
+    it("should redirect to login page when clicking login link", () => {
+        const {enzymeWrapper, props} = setup();
+
+        enzymeWrapper.find("a").simulate("click");
+        expect(props.history.push.calledOnce).toEqual(true);
+    })
+});
